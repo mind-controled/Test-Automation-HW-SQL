60ce44b5f3e56f1b61cc68e061401237c28642bb
mind_control <mind_control@mail.ru>
2020-08-29 14:18:18 +0300

    Initial Commit

---
+++ src/test/java/ru/netology/domain/LoginTest.java
@@ -0,0 +1,51 @@
+package ru.netology.domain;
+
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLUtils;
+import ru.netology.page.LoginPage;
+
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+    SQLUtils mySql = new SQLUtils();
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldLogin() throws SQLException {
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getValidAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = SQLUtils.getVerificationCode(authInfo.getLogin());
+        val verify = verificationPage.verify(verificationCode);
+        verify.checkIfVisible();
+    }
+
+    @Test
+    void shouldBeBlockedAfterThreeWrongPasswords() throws SQLException {
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfoWithInvalidPassword();
+        loginPage.validLogin(authInfo);
+        loginPage.cleanLoginFields();
+        loginPage.validLogin(authInfo);
+        loginPage.cleanLoginFields();
+        loginPage.validLogin(authInfo);
+        val statusSQL = mySql.getStatusFromDb(authInfo.getLogin());
+        assertEquals("blocked", statusSQL);
+    }
+
+    @AfterAll
+    static void clean() throws SQLException {
+        SQLUtils.cleanDb();
+    }
+}
